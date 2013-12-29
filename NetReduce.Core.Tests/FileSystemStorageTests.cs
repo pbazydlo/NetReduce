51f7bb9a91d0753d197728d788cb3624a626ad82
Piotr Bazydło <piotr.bazydlo@student.put.poznan.pl>
2013-12-29 01:24:14 +0100

    Storages clean method covered by tests.

---
+++ NetReduce.Core.Tests/FileSystemStorageTests.cs
@@ -0,0 +1,24 @@
+﻿namespace NetReduce.Core.Tests
+{
+    using System;
+    using System.Linq;
+    using Shouldly;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FileSystemStorageTests
+    {
+        [TestMethod]
+        public void FileSystemStoragCleanMethodDeletesAllFiles()
+        {
+            var storage = new FileSystemStorage(@"c:\temp\netreduce", eraseContents: true);
+            storage.Store("a", "aa");
+            storage.Store("b", "bb");
+
+            storage.Clean();
+            var noOfFiles = storage.ListFiles().Count();
+
+            noOfFiles.ShouldBe(0);
+        }
+    }
+}
---
+++ NetReduce.Core.Tests/InMemoryStorageTests.cs
@@ -0,0 +1,24 @@
+﻿namespace NetReduce.Core.Tests
+{
+    using System;
+    using System.Linq;
+    using Shouldly;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class InMemoryStorageTests
+    {
+        [TestMethod]
+        public void InMemoryStorageCleanMethodDeletesAllFiles()
+        {
+            var storage = new InMemoryStorage();
+            storage.Store("a", "aa");
+            storage.Store("b", "bb");
+
+            storage.Clean();
+            var noOfFiles = storage.ListFiles().Count();
+
+            noOfFiles.ShouldBe(0);
+        }
+    }
+}
