37816fada768a6723016d9ddc7209499228c0392
Zachariusz Karwacki <zachariusz@karwacki.at>
2014-02-07 16:39:22 +0100

    performance monitor?

---
+++ NetReduce.Core.Tests/PerformanceMonitorTests.cs
@@ -0,0 +1,38 @@
+﻿namespace NetReduce.Core.Tests
+{
+    using System.Diagnostics;
+    using System.Threading;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PerformanceMonitorTests
+    {
+        [TestMethod]
+        public void PerformanceCounterTest()
+        {
+            var stop = false;
+            var t = new Thread(
+                () =>
+                    {
+                        while (!stop)
+                        {
+                        }
+                    });
+            t.Start();
+            for (int i = 0; i < 60; i++)
+            {
+                if ((i+1) % 10 == 0)
+                {
+                    stop = !stop;
+                }
+
+                var value = PerformanceMonitor.GetProcessorTimeCounterValue();
+                Debug.WriteLine(value);
+            }
+
+            stop = true;
+            t.Join();
+        }
+    }
+}
---
+++ NetReduce.Core/PerformanceMonitor.cs
@@ -0,0 +1,20 @@
+﻿namespace NetReduce.Core
+{
+    using System.Diagnostics;
+
+    public class PerformanceMonitor
+    {
+        public static float GetProcessorTimeCounterValue()
+        {
+            PerformanceCounter totalProcessorTimeCounter = new PerformanceCounter(
+             "Process",
+             "% Processor Time",
+             /*"_Total"*/ Process.GetCurrentProcess().ProcessName);
+            totalProcessorTimeCounter.NextValue();
+            System.Threading.Thread.Sleep(1000);// 1 second wait
+            var value = totalProcessorTimeCounter.NextValue();
+
+            return value;
+        }
+    }
+}
