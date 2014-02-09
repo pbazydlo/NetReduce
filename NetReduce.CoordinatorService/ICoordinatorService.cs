0da74f2df0ec51ed3adb532154d0ef492d4c65ab
Zachariusz Karwacki <zachariusz@karwacki.at>
2014-02-09 21:10:47 +0100

    CoordinatorService

---
+++ NetReduce.CoordinatorService/ICoordinatorService.cs
@@ -0,0 +1,48 @@
+﻿namespace NetReduce.CoordinatorService
+{
+    using System;
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICoordinatorService" in both code and config file together.
+    [ServiceContract]
+    public interface ICoordinatorService
+    {
+        [OperationContract]
+        void AddWorker(Uri uri);
+
+        [OperationContract]
+        Uri[] GetWorkers();
+
+        [OperationContract]
+        void RemoveWorker(Uri uri);
+
+        [OperationContract]
+        void RunJob(Uri[] filesToProcess, Uri mapCodeFile, Uri reduceCodeFile);
+
+        [OperationContract]
+        Uri AddToStorage(string fileName, string content);
+
+        [OperationContract]
+        Uri[] ListStorageFiles();
+
+        [OperationContract]
+        void RemoveFromStorage(Uri uri);
+
+        [OperationContract]
+        void CleanStorage();
+
+        [OperationContract]
+        MapReduceResult GetResults();
+    }
+
+    [DataContract]
+    public class MapReduceResult
+    {
+        [DataMember]
+        Tuple<string, string>[] KeysAndValues { get; set; }
+
+        [DataMember]
+        bool IsFinished { get; set; }
+    }
+}
---
+++ NetReduce.CoordinatorService/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace NetReduce.CoordinatorService
+{
+    using System;
+    using System.ServiceModel;
+    using System.ServiceModel.Description;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseAddress = new Uri("http://0.0.0.0:7777/CoordinatorService.svc");
+            // Create the ServiceHost.
+            using (var host = new ServiceHost(typeof(CoordinatorService), baseAddress))
+            {
+                // Enable metadata publishing.
+                var smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                host.Description.Behaviors.Add(smb);
+                host.Description.Behaviors.Find<ServiceDebugBehavior>().IncludeExceptionDetailInFaults = true;
+
+                // Open the ServiceHost to start listening for messages. Since
+                // no endpoints are explicitly configured, the runtime will create
+                // one endpoint per base address for each service contract implemented
+                // by the service.
+                host.Open();
+
+                Console.WriteLine("Coordinator service is ready at {0}", baseAddress);
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+
+                // Close the ServiceHost.
+                host.Close();
+            }
+        }
+    }
+}
